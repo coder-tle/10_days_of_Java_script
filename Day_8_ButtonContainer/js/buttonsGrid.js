d37cc6eeea02a052b873689b10f06ea7b8053520
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-12-14 01:05:22 +0530

    Added Code for Button Grid challenge

---
+++ Day_8_ButtonContainer/js/buttonsGrid.js
@@ -0,0 +1,123 @@
+var b1 = document.getElementById('btn1');
+var b2 = document.getElementById('btn2');
+var b3 = document.getElementById('btn3');
+var b4 = document.getElementById('btn4');
+var b5 = document.getElementById('btn5');
+var b6 = document.getElementById('btn6');
+var b7 = document.getElementById('btn7');
+var b8 = document.getElementById('btn8');
+var b9 = document.getElementById('btn9');
+
+
+// updateFunction = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+
+// b1.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b2.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b3.onclick =function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b4.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+b5.onclick = function(){
+     var temp = b1.innerHTML.toString();
+     b1.innerHTML = b4.innerHTML.toString();
+     b4.innerHTML = b7.innerHTML.toString();
+     b7.innerHTML = b8.innerHTML.toString();
+     b8.innerHTML = b9.innerHTML.toString();
+     b9.innerHTML = b6.innerHTML.toString();
+     b6.innerHTML = b3.innerHTML.toString();
+     b3.innerHTML = b2.innerHTML.toString();
+     b2.innerHTML = temp;
+ }
+// b6.onclick =function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b7.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b8.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+// b9.onclick = function(){
+//     var temp = b1.innerHTML.toString();
+//     b1.innerHTML = b4.innerHTML.toString();
+//     b4.innerHTML = b7.innerHTML.toString();
+//     b7.innerHTML = b8.innerHTML.toString();
+//     b8.innerHTML = b9.innerHTML.toString();
+//     b9.innerHTML = b6.innerHTML.toString();
+//     b6.innerHTML = b3.innerHTML.toString();
+//     b3.innerHTML = b2.innerHTML.toString();
+//     b2.innerHTML = temp;
+// }
+
