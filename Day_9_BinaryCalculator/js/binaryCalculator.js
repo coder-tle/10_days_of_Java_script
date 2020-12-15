a61aecf8d5f5d3c2109d5eb0cef136214573d85b
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-12-15 18:49:13 +0530

    Added code for Day 9 binary calculator, code for function of '=' is left

--- Day_9_BinaryCalculator/js/binaryCalculator.js
+++ Day_9_BinaryCalculator/js/binaryCalculator.js
@@ -1,25 +1,74 @@
-var btn0_1 = document.getElementById('btn0');
-var btnClr_1 = document.getElementById('btnClr');
-var btn1_1 = document.getElementById('btn1');
-var res_1 = document.getElementById('res');
+var b0 = document.getElementById('btn0');
+var b1 = document.getElementById('btn1');
+var bC = document.getElementById('btnClr');
+var bR = document.getElementById('res');
+var bE = document.getElementById('btnEql');
+var bSum = document.getElementById('btnSum');
+var bSub = document.getElementById('btnSub');
+var bMul = document.getElementById('btnMul');
+var bDiv = document.getElementById('btnDiv');
 
 
-btnClr_1.onclick = function(){
-   res_1.innerHTML = "";
+
+b0.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str = str + '0';   
+    bR.innerHTML = str;
+}
+
+b1.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str = str + '1';   
+    bR.innerHTML = str;
 }
 
 
-btn0_1.onclick = function(){
-    var str = res_1.innerHTML;
-    str = str + "0";   
-    res_1.innerHTML = str;
+bC.onclick = function(){
+    var str = '';
+    bR.innerHTML = str;
+}
+bSum.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str  = str + '+';
+    bR.innerHTML = str;
+}
+bSub.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str  = str + '-';
+    bR.innerHTML = str;
+}
+
+bMul.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str  = str + '*';
+    bR.innerHTML = str;
 }
 
 
-btn1_1.onclick = function(){
+bDiv.onclick = function(){
+    var str = bR.innerHTML.toString();
+    str  = str + '/';
+    bR.innerHTML = str;
+}
+bE.onclick = function(){
     
-    var str = res_1.innerHTML;
-    str = str + "1";
-    res_1.innerHTML = str;
-}
 
+    const str = bR.innerHTML;
+    const numbers = str.split(/\D/g);
+    const operator = str.split(/\d/g).filter(Boolean);
+
+    console.log(numbers);
+    console.log(operator);
+    var str1 = "Result of expression";
+    bR.innerHTML = str1;
+
+    // var expr = bR.innerHTML;
+    // var nums = /(\d+)/g;
+    // // Replace all base 2 nums with base 10 equivs
+    // expr = expr.replace(nums, function(match) {
+    //     return parseInt(match, 2);
+    // })
+    // // eval in base 10 and convert to base 2
+    // res.innerHTML = eval(expr).toString(2);
+   
+}
